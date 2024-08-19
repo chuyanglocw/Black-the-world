eec6ad001c6f8da674b3a35ebdfa405539adae68
chuyanglocw <2659745557@qq.com>
2024-08-19 20:49:58 +0800

    rest project

--- gdscripts/dath_area.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-
-const END = preload("res://scenes/end.tscn")
-
-func _on_body_entered(body: Node2D) -> void:
-	var end = END.instantiate()
-	get_tree().current_scene.add_child(end)
--- gdscripts/games.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-@onready var ls:= $backgroud/animals/ls
-
-func  _process(delta: float) -> void:
-	pass
